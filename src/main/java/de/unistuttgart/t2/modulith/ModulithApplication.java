a7597a79eaffff4f91589683cb0717dc84d38787
David Kopp <git@davidkopp.de>
2023-10-11 11:35:25 +0200

    Initial commit

---
+++ src/main/java/de/unistuttgart/t2/modulith/ModulithApplication.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.t2.modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModulithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/unistuttgart/t2/modulith/ModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.t2.modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
