e35f3d892fa74af0de200e190d0f4dc5f6728a17
David Kopp <git@davidkopp.de>
2023-10-17 08:49:14 +0200

    Allow trailing slashes (temporarily)

---
+++ src/main/java/de/unistuttgart/t2/modulith/WebConfiguration.java
@@ -0,0 +1,17 @@
+package de.unistuttgart.t2.modulith;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        // Method `setUseTrailingSlashMatch` is deprecated.
+        // See https://github.com/spring-projects/spring-framework/issues/28552
+        // TODO Check UI to not use URLs with a trailing slash
+        configurer.setUseTrailingSlashMatch(true);
+    }
+}
