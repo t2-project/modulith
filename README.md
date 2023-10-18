3ac3eaebf0fe7788c7db5f1334c1edc29e725de7
David Kopp <git@davidkopp.de>
2023-10-18 14:53:12 +0200

    Add Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# T2-Modulith
+
+This project is currently *work in progress*.
+
+The T2-Modulith is an implementation of the T2-Project as a monolith, however, in a modular way.
--- src/main/java/de/unistuttgart/t2/modulith/config/OpenAPIConfiguration.java
+++ src/main/java/de/unistuttgart/t2/modulith/config/OpenAPIConfiguration.java
@@ -23,8 +23,8 @@ public class OpenAPIConfiguration {
     @Bean
     public io.swagger.v3.oas.models.OpenAPI customOpenAPI() {
         return new io.swagger.v3.oas.models.OpenAPI().components(new Components()).info(new Info()
-                .title("UIBackend service API")
-                .description("API of the T2-Project's UIbackend service.")
+                .title("T2 Modulith API")
+                .description("API of the T2-Project's modulith implementation.")
                 .version(version));
     }
 }
