a795bd7d635e642cc34c193ab8a2891e2cda132c
David Kopp <git@davidkopp.de>
2024-11-16 10:43:01 +0100

    Remove unimplemented cart and inventory rest endpoints

---
+++ src/main/java/de/unistuttgart/t2/modulith/config/SpringDataRestConfig.java
@@ -0,0 +1,24 @@
+package de.unistuttgart.t2.modulith.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy.RepositoryDetectionStrategies;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class SpringDataRestConfig {
+
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+
+        return new RepositoryRestConfigurer() {
+
+            @Override
+            public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+                config.setRepositoryDetectionStrategy(RepositoryDetectionStrategies.ANNOTATED);
+            }
+        };
+    }
+}
